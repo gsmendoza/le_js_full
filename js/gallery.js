d5203a6ce2736d1fd1c49bb9483329a293f40925
George Mendoza <gsmendoza@gmail.com>
2019-03-15 16:43:45 +0800

    WIP

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,12 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+      mainImage.setAttribute("alt", thumbnail.dataset.description);
+    });
+  });
 }
