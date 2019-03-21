7d4af31a9f478fedb0def8fdb6bfeef069249c4f
George Mendoza <gsmendoza@gmail.com>
2019-03-21 16:36:42 +0800

    Set thumbnail clicked as current

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,17 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-
   let mainImage = document.querySelector("#gallery-photo > img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
       mainImage.setAttribute("alt", thumbnail.dataset.description);
+
+      let currentClass = "current";
+
+      // Change which image is current.
+      document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
     });
   });
 }
